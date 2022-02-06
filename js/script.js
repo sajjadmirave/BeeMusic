88a4b12ca6ff5996cd5fe2d1b65beae7c29a4268
sajjad <banditofsocorstars@gmail.com>
2022-02-06 03:30:39 -0800

    end responsive on mobile whene click on the bars

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-function openFunction(){
-    document.getElementById("aside-menu").style.width = "165px"
-    document.getElementById("openFunction").style.left = "175px"
+function openFunction() {
+  let aside_menu = document.getElementById("aside-menu").value;
+
+  document.getElementById("aside-menu").style.width = "165px";
+  document.getElementById("openFunction").style.left = "175px";
+  // document.getElementById("aside-menu-data").style.display = "block"
+  if ((document.getElementById("aside-menu").style.width = "165px")) {
+    document.getElementById("aside-menu-data").style.transition = "all 2s";
+    document.getElementById("aside-menu-data").style.display = "block";
+  }
 }
 
-function closeFunction(){
-    document.getElementById("aside-menu").style.width = "0px"
-    document.getElementById("openFunction").style.left = "10px"
-
+function closeFunction() {
+  document.getElementById("aside-menu").style.width = "0px";
+  document.getElementById("openFunction").style.left = "10px";
+  document.getElementById("aside-menu-data").style.display = "none";
 }
