5a4ce4bc63c2952320227a3c4f845ce342cee0b7
sajjad <banditofsocorstars@gmail.com>
2022-02-06 01:47:13 -0800

    create aside menu and responsive theme

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openFunction(){
+    document.getElementById("aside-menu").style.width = "165px"
+    document.getElementById("openFunction").style.left = "175px"
+}
+
+function closeFunction(){
+    document.getElementById("aside-menu").style.width = "0px"
+    document.getElementById("openFunction").style.left = "10px"
+
+}
